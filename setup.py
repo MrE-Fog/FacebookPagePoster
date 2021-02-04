0b32596124d31ec8496463424b1542d03adadd50
Matheus Horstmann <horstmannmat@hotmail.com>
2021-02-04 00:18:02 -0300

    Update pip version
    Signed-off-by: Matheus Horstmann <horstmannmat@hotmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="FacebookPagePoster",
-    version="0.1.9",
+    version="0.1.10",
     author="Matheus Horstmann",
     author_email="mch15@c3sl.ufpr.br",
     description="A bot to post of facebook pages",
